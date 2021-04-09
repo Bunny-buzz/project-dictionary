b561c74fa5fe466de12331c38d5192a564455b12
Bunny-buzz <Bunny-buzz@users.noreply.github.com>
2021-04-09 15:53:22 -0600

    added new files(dictionary.js & css, ), react, and a logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
+import logo from "./logo.png";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contianer">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="footer text-center">
+          <small>
+            coded by Nimi Dada, open-sourced <a href="#">Github</a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Dictionary.js
@@ -0,0 +1,21 @@
+import react, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [word, setWord] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`searching for ${word}`);
+  }
+  function handleWordChange(event) {
+    console.log(event.target.value);
+    setWord(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleWordChange} autoFocus={true} />
+      </form>
+    </div>
+  );
+}
