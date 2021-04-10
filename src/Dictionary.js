960386ade6a03cd891dca865fc65b3602bdc5dad
Bunny-buzz <Bunny-buzz@users.noreply.github.com>
2021-04-09 20:07:27 -0600

    added api url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,6 +11,8 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
+    // documentation: https://dictionaryapi.dev/
+
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
     console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
