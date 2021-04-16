96ca8a84034f966dd3a6889a379607ca1c1cfe4d
Bunny-buzz <Bunny-buzz@users.noreply.github.com>
2021-04-16 16:11:58 -0600

    made it responsive and addes photo component

--- src/App.js
+++ src/App.js
@@ -5,29 +5,26 @@ import logo from "./logo.png";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App ">
       <div className="contianer">
-        <br />
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
         <main>
-          <Dictionary defaultKeyword="Serendipity" />
+          <Dictionary defaultKeyword="Sunset" />
         </main>
-
-        <footer className="footer text-center">
-          <small>
-            coded by Nimi Dada, open-sourced{" "}
-            <a href="https://github.com/Bunny-buzz/project-dictionary">
-              Github
-            </a>{" "}
-            and hosted on{" "}
-            <a href="https://condescending-montalcini-7952ff.netlify.app/">
-              Netlify
-            </a>
-          </small>
-        </footer>
       </div>
+      <footer className="footer text-center">
+        <small>
+          coded by <a href="https://nimidada.netlify.app/">Nimi Dada</a>,
+          open-sourced{" "}
+          <a href="https://github.com/Bunny-buzz/project-dictionary">Github</a>{" "}
+          and hosted on{" "}
+          <a href="https://condescending-montalcini-7952ff.netlify.app/">
+            Netlify
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Results from "./Results";
+import Photos from "./Photos";
 import axios from "axios";
 import "./Dictionary.css";
 
@@ -7,17 +8,24 @@ export default function Dictionary(props) {
   let [word, setWord] = useState(props.defaultKeyword);
   let [meaning, setMeaning] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
-    console.log(response.data[0]);
+  function handleDictionaryResponse(response) {
     setMeaning(response.data[0]);
   }
 
+  function handlePixabayResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.hits[0]);
+  }
   function search() {
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
-    console.log(apiUrl);
-    axios.get(apiUrl).then(handleResponse);
+
+    axios.get(apiUrl).then(handleDictionaryResponse);
+    let pixabayApiKey = "21195821-1a42fea634dbe86cb953ddd60";
+    let pixabayApiUrl = `https://pixabay.com/api/?key=21195821-1a42fea634dbe86cb953ddd60&q=${word}&image_type=photo&api_key=${pixabayApiKey}`;
+    axios.get(pixabayApiUrl).then(handlePixabayResponse);
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -25,7 +33,6 @@ export default function Dictionary(props) {
   }
 
   function handleWordChange(event) {
-    console.log(event.target.value);
     setWord(event.target.value);
   }
 
@@ -38,6 +45,7 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
+          <h1>What word are you searching for?</h1>
           <form onSubmit={handleSubmit}>
             <input
               type="search"
@@ -50,7 +58,9 @@ export default function Dictionary(props) {
             search words: serendipty, happiness, determination....
           </div>
         </section>
+
         <Results results={meaning} />
+        <Photos results={photos} />
       </div>
     );
   } else {
