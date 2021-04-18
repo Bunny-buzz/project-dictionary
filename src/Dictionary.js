288ee430c80c45a07d285218d5363f3427e31dc5
Bunny-buzz <Bunny-buzz@users.noreply.github.com>
2021-04-17 20:49:10 -0600

    reworked Photo and Dictionary.js and added some styling to photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,25 +3,21 @@ import Results from "./Results";
 import Photos from "./Photos";
 import axios from "axios";
 import "./Dictionary.css";
-
 export default function Dictionary(props) {
   let [word, setWord] = useState(props.defaultKeyword);
   let [meaning, setMeaning] = useState(null);
   let [loaded, setLoaded] = useState(false);
   let [photos, setPhotos] = useState(null);
-
   function handleDictionaryResponse(response) {
     setMeaning(response.data[0]);
   }
-
   function handlePixabayResponse(response) {
     console.log(response.data);
-    setPhotos(response.data.hits[0]);
+    setPhotos(response.data.hits);
   }
   function search() {
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
-
     axios.get(apiUrl).then(handleDictionaryResponse);
     let pixabayApiKey = "21195821-1a42fea634dbe86cb953ddd60";
     let pixabayApiUrl = `https://pixabay.com/api/?key=21195821-1a42fea634dbe86cb953ddd60&q=${word}&image_type=photo&api_key=${pixabayApiKey}`;
@@ -31,16 +27,13 @@ export default function Dictionary(props) {
     event.preventDefault();
     search();
   }
-
   function handleWordChange(event) {
     setWord(event.target.value);
   }
-
   function load() {
     setLoaded(true);
     search();
   }
-
   if (loaded) {
     return (
       <div className="Dictionary">
@@ -58,9 +51,8 @@ export default function Dictionary(props) {
             search words: serendipty, happiness, determination....
           </div>
         </section>
-
         <Results results={meaning} />
-        <Photos results={photos} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
--- src/Photos.js
+++ src/Photos.js
@@ -3,18 +3,26 @@ import "./Photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
-    console.log(props.photos);
     return (
       <section className="Photos">
-        {props.photos.map(function (photos, index) {
-          return;
-
-          <img src={photos.largeImageUrl} />;
-        })}
+        <div className="row">
+          {props.photos.map(function (photos, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photos.largeImageURL} target="_blank" rel="noreferrer">
+                  <img
+                    src={photos.largeImageURL}
+                    alt=""
+                    className="img-fluid"
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
       </section>
     );
   } else {
     return null;
   }
 }
-//https://pixabay.com/get/gb8c0c9ec9e95aedd182557e2efb1576f0f24fcd66395df49a87ad9f443ef403286bca3402f44dc6cd89fc916fcd237c4aa2489f7ee255504143c1cb250b81fd0_1280.jpg
